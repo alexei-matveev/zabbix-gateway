0a1ca1662a37c28cb9af51ec5f1bd0c5109b220e
Alexei Matveev <alexei.matveev@gmail.com>
2018-07-05 15:07:56 +0200

    AM: cider has problems with 1.9

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.7.0-RC1"]
                  [ring/ring-jetty-adapter "1.7.0-RC1"]
                  [ring/ring-json "0.4.0"]
