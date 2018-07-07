5eb5c5c0245e4644b2d2b593a9ef7c55088e6254
Alexei Matveev <alexei.matveev@gmail.com>
2018-07-07 22:25:22 +0200

    AM: id does work with recent cider

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 ;; See the sample project file for other options:
 ;; https://github.com/technomancy/leiningen/blob/master/sample.project.clj
-(defproject zabbix-gateway "0.1.0-SNAPSHOT"
+(defproject f0bec0d/zabbix-gateway "0.1.0-SNAPSHOT"
   :description "HTTP to Zabbix Sender/Trapper"
   :url "https://github.com/alexei-matveev/zabbix-gateway"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.cli "0.3.7"]
                  [ring/ring-core "1.7.0-RC1"]
                  [ring/ring-jetty-adapter "1.7.0-RC1"]
